3946df45e27a8295ee8eac14d1f952e44f085c2d
Pavel.Malko <pavel.malko@ksyste.ms>
2019-08-05 18:56:48 +0300

    init commit

---
+++ app/src/main/java/ua/pavel/malko/android/zimadtestapp/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ua.pavel.malko.android.zimadtestapp.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ua.pavel.malko.android.zimadtestapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/ua/pavel/malko/android/zimadtestapp/ui/ZiMadApp.java
@@ -0,0 +1,6 @@
+package ua.pavel.malko.android.zimadtestapp.ui;
+
+import android.app.Application;
+
+public class ZiMadApp extends Application {
+}
